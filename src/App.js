5cbaf6d4eb2c573707f81eec980be390c52650f4
Nina Proctor <nina.proctor@mykenzie.snhu.edu>
2022-12-20 10:30:16 -0500

    Working on useEffect in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { useEffect } from 'react';
+
 
 function App() {
+
+
+useEffect(() => {
+ const socket = io()
+}, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div> Hello socket 
+    <Button varient='text'>Text</Button>
+
+
+   </div>
+   
   );
 }
 
