24572ab00d8f6156236b6656230ec86a51147c88
Nina Proctor <nina.proctor@mykenzie.snhu.edu>
2022-12-22 10:12:43 -0500

    Working on messaging core

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
-import { useEffect } from 'react';
-
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box'
+import { useEffect, useState } from 'react';
+import {io} from 'socket.io-client'
 
 function App() {
-
-
+  const [socket, setSocket] = useState(null)
+ 
 useEffect(() => {
- const socket = io()
+  setSocket = (io("http://localhost:4000"))
 }, [])
 
+function handleForm(e) {
+e.preventDefault()
+socket.emit('send-message')
+setMessage('')
+}
 
 
   return (
-  <div> Hello socket 
-    <Button varient='text'>Text</Button>
+  <div>   
+    <Box component="form" onSubmit={handleForm}>
+      <TextField 
+      size="small"
+      id="stadard-basic" 
+      label="Write your message here" 
+      variant="standard"   
+      value={message}
+      onChange={(e) => setMessage(e.target.value)}
+      
+      />
+    <Button varient="text" type="submit">
+      Send
+    </Button>
+    </Box>
+    
 
 
    </div>
